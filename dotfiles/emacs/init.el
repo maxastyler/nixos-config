bdcf51f30a39b06c80eabfe30191bdb78060d887
Max Tyler <maxastyler@gmail.com>
2019-07-22 12:31:14 +0100

    Add emacs configuration files

---
+++ dotfiles/emacs/init.el
@@ -0,0 +1,25 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; add the melpa repository 
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+;; Initialize package
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Autoload use-package
+(eval-when-compile
+  (require 'use-package))
+
+;; Load the literate configuration
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
