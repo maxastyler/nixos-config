42c09a55f993eddb18238f1459a90db3360f95c1
maxastyler <maxastyler@gmail.com>
2022-09-11 22:22:32 +0100

    Test signing

--- configuration.nix
+++ configuration.nix
@@ -118,14 +118,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
-    pass
-    (firefox-wayland.override {extraNativeMessagingHosts = [passff-host];})
-    libvterm
-    cmake
-    gcc
-  #  wget
+    #  wget
   ];
 
   services.emacs = {
@@ -134,13 +129,14 @@
     defaultEditor = true;
   };
 
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # List services that you want to enable:
 
--- home.nix
+++ home.nix
@@ -1,13 +1,38 @@
-{ home-manager, pkgs, ... }:
+{config,  home-manager, pkgs, ... }:
+let   xserverCfg = config.services.xserver;
+      
+  defaultPinentryFlavor =
+    if xserverCfg.desktopManager.lxqt.enable
+    || xserverCfg.desktopManager.plasma5.enable then
+      "qt"
+    else if xserverCfg.desktopManager.xfce.enable then
+      "gtk2"
+    else if xserverCfg.enable || config.programs.sway.enable then
+      "gnome3"
+    else
+      "curses";
+  in
 {
   imports = [home-manager.nixosModules.home-manager];
   home-manager.users.max = {
     home.stateVersion = "22.05";
+    home.packages = with pkgs; [
+      pass
+      (firefox-wayland.override {extraNativeMessagingHosts = [passff-host];})
+      libvterm
+      cmake
+      gcc
+      nixfmt
+    ];
     programs = {
       git = {
         enable = true;
         userName = "maxastyler";
         userEmail = "maxastyler@gmail.com";
+        signing = {
+          signByDefault = true;
+          key = null;
+        };
       };
       bash = {
         enable = true;
@@ -19,6 +44,25 @@
         enableBashIntegration = true;
         nix-direnv.enable = true;
         stdlib = builtins.readFile ./direnvrc;
+      };
+      gpg = {
+        enable=true;
+      };
+    };
+    services = {
+      emacs = {
+        enable = true;
+        package = with pkgs; (emacsPackagesFor emacsPgtkNativeComp).emacsWithPackages (epkgs: [epkgs.vterm epkgs.pdf-tools]);
+        defaultEditor = true;
+        client.enable = true;
+      };
+      gpg-agent = {
+        enable=true;
+        enableSshSupport = true;
+        pinentryFlavor = defaultPinentryFlavor;
+        sshKeys = [
+          "3FB4D32DB79704F31D7356358F826B4D2EEF8DD3"
+        ];
       };
     };
   };
