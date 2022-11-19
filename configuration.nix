4fd872a29368ba6b17acdb9c2e0f6466cf4cc879
maxastyler <maxastyler@gmail.com>
2022-11-19 09:50:59 +0000

    Enable openssh

--- configuration.nix
+++ configuration.nix
@@ -129,7 +129,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # use adb
   programs.adb.enable = true;
